b6fa7bcf0a1881dbbf5055e97309cc1ab641db5c
ltc <jonh_ltc@163.com>
2021-03-09 20:57:23 +0800

    第一次提交

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,d;
+    a=-1;
+    b=-2;
+    c=(a+b)/2;
+    d=4;
+    printf("a=%d,b=%d,c=%d,%d",a,b,c,d);
+    return 0;
+}
